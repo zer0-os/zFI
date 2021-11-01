afe0f60dbed37ad611aff490e3098fab67b190f6
James Earle <j_earle@hotmail.com>
2021-11-01 14:01:59 -0700

    working checkpoint, beginning tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,6 +14,10 @@ const config: HardhatUserConfig = {
       {
         version: "0.8.4",
         settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          },
           outputSelection: {
             "*": {
               "*": ["storageLayout"],
