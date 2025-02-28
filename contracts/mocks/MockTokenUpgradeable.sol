dbe4da068dd68da6ab382dd87bb01941f596d371
Whytecrowe <dor7an@gmail.com>
2025-02-27 17:31:39 -0800

    fix mangled mock token names

--- contracts/mocks/MockTokenUpgradeable.sol
+++ contracts/mocks/MockTokenUpgradeable.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
-contract MockToken is Initializable, ERC20Upgradeable {
+contract MockTokenUpgradeable is Initializable, ERC20Upgradeable {
   function initialize() public initializer {
     __ERC20_init("Mock wrapped Ethereum", "MwETH");
   }
